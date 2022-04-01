df3941d459a6bbbb594cbb75d6cd55fe6df75aef
1904477 <59955897+1904477@users.noreply.github.com>
2022-04-01 15:43:17 +0100

    Porting almost done.

---
+++ Help_Mark_Escape/Intermediate/Source/Help_Mark_Escape.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Help_Mark_Escape : ModuleRules
+{
+	public Help_Mark_Escape(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
